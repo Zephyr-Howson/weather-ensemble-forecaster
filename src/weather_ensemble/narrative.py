3cded59bc969b84de230d4174f33bc4b858e73a1
Zephyr-Howson <zephyr.howson@roymorgan.com>
2026-08-05 09:23:06 +1000

    Prompt update

--- src/weather_ensemble/narrative.py
+++ src/weather_ensemble/narrative.py
@@ -66,7 +66,7 @@ SYSTEM_PROMPT = (
     
     Wind:
     Below 11 km/h wind speed - calm
-    12-19 km/h wind speed - pleasant breeze
+    12-19 km/h wind speed - slightly breezy
     20-28 km/h wind speed - breezy
     29-38 km/h wind speed - windy
     39-61 km/h wind speed - very windy
